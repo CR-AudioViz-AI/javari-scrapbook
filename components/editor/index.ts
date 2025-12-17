12f3a1fbcdffdfdbc0298228a072c74a9751c491
Old <225056366+roy-henderson@users.noreply.github.com>
2025-12-16 20:47:21 -0500

    feat: Export all 21 editor components from index

--- components/editor/index.ts
+++ components/editor/index.ts
@@ -1,6 +1,37 @@
-// CRAV Scrapbook - Editor Components Export
-export { EditorCanvas } from './EditorCanvas';
-export { EditorToolbar } from './EditorToolbar';
-export { AssetsPanel } from './AssetsPanel';
-export { PropertiesPanel } from './PropertiesPanel';
-export { PagesPanel } from './PagesPanel';
+// components/editor/index.ts
+// Export all editor components for easy importing
+
+// Core Editor Components
+export { default as EditorCanvas } from './EditorCanvas';
+export { default as EditorToolbar } from './EditorToolbar';
+export { default as PropertiesPanel } from './PropertiesPanel';
+export { default as PagesPanel } from './PagesPanel';
+export { default as EnhancedPagesPanel } from './EnhancedPagesPanel';
+
+// Asset Browsers
+export { default as AssetsPanel } from './AssetsPanel';
+export { default as StockPhotoBrowser } from './StockPhotoBrowser';
+export { default as GiphyBrowser } from './GiphyBrowser';
+export { default as TemplateGallery } from './TemplateGallery';
+
+// NEW: Creative Asset Browsers
+export { default as ShapesBrowser } from './ShapesBrowser';
+export { default as StickersBrowser } from './StickersBrowser';
+export { default as FiltersBrowser } from './FiltersBrowser';
+export { default as FramesBrowser } from './FramesBrowser';
+
+// NEW: Generators & Pickers
+export { default as QRCodeGenerator } from './QRCodeGenerator';
+export { default as AvatarCreator } from './AvatarCreator';
+export { default as CollageBuilder } from './CollageBuilder';
+export { default as BackgroundPicker } from './BackgroundPicker';
+export { default as PatternPicker } from './PatternPicker';
+export { default as ColorPalettePicker } from './ColorPalettePicker';
+export { default as TextEffectsPanel } from './TextEffectsPanel';
+
+// AI & Collaboration
+export { default as AIEnhancePanel } from './AIEnhancePanel';
+export { default as CollaborationPanel } from './CollaborationPanel';
+
+// Export Modal
+export { default as ExportModal } from './ExportModal';
